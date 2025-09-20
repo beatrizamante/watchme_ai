5ea2ffc27ddbb00b6756b8f08507e8f374aaabad
beatrizamante <beatrizamante@hotmail.com>
2025-09-20 19:35:27 -0300

    Setting off new parameters

--- src/infrastructure/scripts/split_dataset.py
+++ src/infrastructure/scripts/split_dataset.py
@@ -3,10 +3,10 @@ import random
 import shutil
 from pathlib import Path
 
-def split_dataset(images_dir, labels_dir, output_dir, train_ratio=0.7, val_ratio=0.15, test_ratio=0.15):
+def split_dataset(images_dir, labels_dir, output_dir, train_ratio=0.7, val_ratio=0.15):
     for split in ["train", "val", "test"]:
         (Path(output_dir) / "images" / split).mkdir(parents=True, exist_ok=True)
-        (Path(output_dir) / "labels" / split).mkdir(parents=True, exist_ok=True)
+        (Path(output_dir) / "annotations" / split).mkdir(parents=True, exist_ok=True)
 
     images = [f for f in os.listdir(images_dir) if f.endswith((".jpg", ".png"))]
     random.shuffle(images)
@@ -26,5 +26,4 @@ def split_dataset(images_dir, labels_dir, output_dir, train_ratio=0.7, val_ratio
             label_file = img_file.replace(".jpg", ".txt").replace(".png", ".txt")
             
             shutil.copy(os.path.join(images_dir, img_file), Path(output_dir) / "images" / split / img_file)
-            shutil.copy(os.path.join(labels_dir, label_file), Path(output_dir) / "labels" / split / label_file)
-
+            shutil.copy(os.path.join(labels_dir, label_file), Path(output_dir) / "annotations" / split / label_file)
--- src/infrastructure/yolo/core/train.py
+++ src/infrastructure/yolo/core/train.py
@@ -1,12 +1,15 @@
 from config import settings
 from ..client.client import yolo_client
 
-##Kerasturner
-
 def train():
     results = yolo_client.train(
         data="src/dataset/yolo/dataset.yaml",
         epochs=settings.YOLO_EPOCHS,
-        imgsz=settings.YOLO_IMAGE_SIZE
-    )
+        batch=settings.YOLO_IMAGE_SIZE,
+        optimizer=settings.YOLO_LOSS_FUNC,
+        lr0=settings.YOLO_LEARNING_RATE,
+        dropout=settings.YOLO_DROPOUT,
+        imgsz=640   
+        )
+       
     return results
