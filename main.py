50dfee595e3397f8942bdab099f62e0751319096
beatrizamante <beatrizamante@hotmail.com>
2025-11-16 08:59:46 -0300

    Fixed distance issue

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from src.config.logging_config import setup_logging
 
 from src.interface.http.routes.person_embedding_router import router as person_router
-from src.interface.http.routes.test_route import router as test_router
 
 from src.interface.http.server import make_server
 from src.interface.websocket.websocket_protocol import ws_router
@@ -12,7 +11,6 @@ logger = setup_logging()
 app = FastAPI()
 
 app.include_router(person_router)
-app.include_router(test_router)
 app.include_router(ws_router)
 
 if __name__ == "__main__":
